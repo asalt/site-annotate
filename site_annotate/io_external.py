70d0cdd6eb7307b7845dc22a2d5561fb85433ffb
asalt <a.saltzman920@gmail.com>
2024-06-04 16:34:04 -0500

    build out file loading routine, with tests

--- site_annotate/io_external.py
+++ site_annotate/io_external.py
@@ -3,18 +3,34 @@ from pathlib import Path
 import requests
 import pandas as pd
 
+import janitor
+
 from . import log
 
-# Path of the current script
-current_script_path = Path(__file__).resolve()
-
-# Path to the top-level data directory
-data_dir = current_script_path.parent.parent / 'data'
-
-print(data_dir)
+logger = log.get_logger(__file__)
 
 
-def check_files_exist(file_names, directory='data'):
+def set_data_dir():
+    # Path of the current script
+    current_script_path = Path(__file__).resolve()
+    # Path to the top-level data directory
+    res = current_script_path.parent.parent / "data"
+    logger.debug(f"data dir set to {res}")
+    return res
+
+
+data_dir = set_data_dir()
+
+
+# print(data_dir)
+
+FILE_CHOCIES = [
+    "Phosphorylation_site_dataset",
+    "Regulatory_sites",
+]
+
+
+def check_files_exist(file_names, directory=data_dir):
     """Check if all required files exist in the specified directory."""
     missing_files = []
     for file_name in file_names:
@@ -25,7 +41,8 @@ def check_files_exist(file_names, directory='data'):
         return False
     return True
 
-def load_data(file_name, directory='data'):
+
+def load_data(file_name, directory="data"):
     """Load data from a specified file within the given directory."""
     file_path = os.path.join(directory, file_name)
     if os.path.exists(file_path):
@@ -35,12 +52,24 @@ def load_data(file_name, directory='data'):
         print(f"File not found: {file_name}")
         return None
 
-# Example Usage
-required_files = ['phosphosite_data.csv', 'another_data_file.csv']
-if check_files_exist(required_files):
-    for file_name in required_files:
-        data = load_data(file_name)
-        # Process data as needed
-else:
-    print("Please ensure all required files are in the data directory.")
 
+def get_psiteplus_file(file, **kwargs):
+
+    skiprows = 3
+    if "skiprows" in kwargs:
+        skiprows = kwargs.pip("skiprows")
+
+    fullfile = data_dir / file
+    df = pd.read_table(fullfile, skiprows=skiprows, **kwargs)
+    df = janitor.clean_names(df)
+    return df
+
+
+# # Example Usage
+# required_files = ['phosphosite_data.csv', 'another_data_file.csv']
+# if check_files_exist(required_files):
+#     for file_name in required_files:
+#         data = load_data(file_name)
+#         # Process data as needed
+# else:
+#     print("Please ensure all required files are in the data directory.")
--- tests/test_io_external.py
+++ tests/test_io_external.py
@@ -1,5 +1,6 @@
 # tests/test_io_ext.py
 import os
+from pathlib import Path
 import pytest
 from io import StringIO
 from pathlib import Path
@@ -8,6 +9,48 @@ from click.testing import CliRunner
 import site_annotate.io_external
 from pytest_mock import MockerFixture
 
+
 def test_module_load():
     dir(site_annotate.io_external)
 
+
+def test_get_data_dir():
+    res = site_annotate.io_external.set_data_dir()
+    res = Path(res)
+
+    assert res.resolve() == Path("../data/").resolve()
+
+
+def test_check_files_exists():
+    res = site_annotate.io_external.check_files_exist(["Phosphorylation_site_dataset"])
+    assert res == True
+
+
+def test_load_file():
+    file = "Phosphorylation_site_dataset"
+
+    res = site_annotate.io_external.check_files_exist([file])
+    if res == False:
+        pytest.skip(f"file : {file} not found, skipping")
+    expected_names = [
+        "gene",
+        "protein",
+        "acc_id",
+        "hu_chr_loc",
+        "mod_rsd",
+        "site_grp_id",
+        "organism",
+        "mw_kd",
+        "domain",
+        "site_+_7_aa",
+        "lt_lit",
+        "ms_lit",
+        "ms_cst",
+        "cst_cat#",
+        "ambiguous_site",
+    ]
+
+    df = site_annotate.io_external.get_psiteplus_file(file, nrows=10)
+
+    for name in expected_names:
+        assert name in df.columns
