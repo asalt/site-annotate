3bfdda2ca792b4bb4cde4a35375d4f8687f9f469
Alex Saltzman <saltzman@bcm.edu>
2025-01-27 13:51:06 -0600

    refactor heatmap drawing into smaller funcs

--- R/run.R
+++ R/run.R
@@ -1,31 +1,46 @@
+# run.R
 # main entry point for all downstream analyses
+
+source(file.path("setup_environment.R"))
+source(file.path("lazyloader.R"))
+
+cache_tools <- get_tool_env("cache")
+`%cached_by%` <- cache_tools$`%cached_by%`
+
+
 
 process_gct_file <- function(gct_file, config) {
 
-  if (!file.exists(gct_file)) {
-    message(paste0("File ", gct_file, " does not exist, exiting"))
-    stop()
-  }
 
-  gct <- cmapR::parse_gctx(gct_file)
   util_tools <- get_tool_env("utils")
 
-  if (config$norm$normalize %||% FALSE) {
-    gct <- util_tools$normalize_gct(gct, log_transform = config$norm$log_transform %||% TRUE)
+  if (!file.exists(gct_file)) {
+    stop(paste0("File ", gct_file, " does not exist, exiting"))
   }
 
-  gct <- util_tools$filter_nonzeros(
-    gct,
-    config$filter$non_zeros %||% 1.0,
-    config$filter$nonzero_subgroup %||% NULL
-  )
+  # Step 1: Load or Parse GCT File
+  #gct <- load_or_parse_gct(gct_file)
+  gct <- parse_gctx(gct_file) %cached_by% rlang::hash_file(gct_file)
 
+  # Step 2: Normalize GCT
+  norm_config <- config$norm
+  gct <- util_tools$normalize_gct(gct, log_transform = norm_config$log_transform %||% TRUE) %cached_by%
+    rlang::hash(c(gct, norm_config))
+
+  # Step 3: Filter Non-Zeros
+  gct <- util_tools$filter_nonzeros(gct, config$filter$non_zeros %||% 1.0,
+    config$filter$nonzero_subgroup %||% NULL
+  ) %cached_by% rlang::hash(c(gct, config$filter))
+
+  # Step 4: Apply Batch Correction (if required)
+  # need more error checking ehre or inside
   if (is.character(config$norm$batch) %||% FALSE) {
-    gct <- util_tools$do_combat(gct, by = config$norm$batch)
+    gct <- util_tools$do_combat(gct, by = config$norm$batch) %cached_by% rlang::hash(c(gct, config$norm$batch))
   }
 
   return(gct)
 }
+
 
 exclude_samples <- function(gct, sample_exclude) {
   if (!is.null(sample_exclude)) {
@@ -43,6 +58,166 @@ load_and_validate_config <- function(config_file, data_dir) {
   if (is.null(config)) stop("Config file could not be loaded.")
   return(config)
 }
+
+
+# Main function to handle heatmap generation
+generate_global_heatmaps <- function(gct_z, config, outdir, friendly_name, outname) {
+
+  param_grid <- expand.grid(
+    cut_by = unique(c(config$heatmap$cut_by %||% NA, NA)),
+    cluster_columns = c(TRUE, FALSE),
+    stringsAsFactors = FALSE
+  )
+
+  # Step 2: Iterate over parameter grid and generate heatmaps
+  param_grid %>%
+    purrr::pmap(~ generate_global_heatmap(
+      gct_z = gct_z,
+      cut_by = ..1,
+      cluster_columns = ..2,
+      config = config,
+      outdir = outdir,
+      friendly_name = friendly_name,
+      outname = outname
+    ))
+
+}
+
+# Helper: Generate a single heatmap
+generate_global_heatmap <- function(gct_z, cut_by, cluster_columns, config, outdir, friendly_name, outname) {
+  # Prepare the subset GCT
+  rids_to_keep <- gct_z@rdesc %>%
+    distinct(fifteenmer, .keep_all = TRUE) %>%
+    pull(id)
+
+  gct_z_subset <- gct_z %>% subset_gct(rid = rids_to_keep)
+
+  # Generate heatmap code
+  heatmap_tools <- get_tool_env("heatmap")
+  ht_draw_code <- gct_z_subset %>% heatmap_tools$make_heatmap_fromgct(
+    show_row_names = FALSE,
+    optimal_size = FALSE,
+    cut_by = cut_by,
+    cluster_columns = cluster_columns,
+    include_row_annotations = FALSE,
+    meta_to_include = config$heatmap$legend_include %||% NULL,
+    meta_to_exclude = config$heatmap$legend_exclude %||% NULL,
+    column_title = friendly_name
+  )
+
+  # Prepare file paths
+  outf <- heatmap_tools$prepare_heatmap_output_path(outdir,
+    "heatmap",
+    outname,
+    dimensions = list(nrow=nrow(gct_z_subset@mat), ncol(gct_z_subset@mat)),
+    cut_by = cut_by,
+    cluster_rows = T,
+    cluster_columns = cluster_columns
+  )
+
+  # Skip if file exists and replace is FALSE
+  if (file.exists(outf) && config$advanced$replace == FALSE) {
+    message(paste0(outf, " already exists, skipping"))
+    return(NULL)
+  }
+
+  # Draw and save the heatmap
+  heatmap_tools$draw_and_save_heatmap(ht_draw_code, outf, gct_z_subset, width=NULL, height=11)
+}
+
+generate_selection_heatmaps <- function(gct_z, config, outdir, friendly_name = '', outname = '') {
+  io_tools <- get_tool_env("io")
+  heatmap_tools <- get_tool_env("heatmap")
+
+
+  param_grid <- expand.grid(
+    cut_by = config$heatmap$selection$cut_by %||% config$heatmap$cut_by %||% NA,
+    cluster_rows = config$heatmap$selection$cluster_rows %||% config$heatmap$cluster_rows %||% TRUE,
+    cluster_columns = config$heatmap$selection$cluster_columns %||% config$heatmap$cluster_columns %||% c(TRUE, FALSE),
+    stringsAsFactors = FALSE
+  )
+
+  file_list <- config$heatmap$selection$file_list
+  if (is.null(file_list)) {
+    message("No file list provided for selection heatmaps, skipping")
+    return(NULL)
+  }
+
+  config$heatmap$selection$file_list %>%
+    purrr::list_flatten() %>%
+    purrr::imap(~ {
+      the_file <- .x
+      file_name <- .y
+
+      # Read and subset GCT
+      file_data <- io_tools$read_genelist_file(the_file)
+      subgct_z <- gct_z %>% subset_gct(rid = file_data$id %||% file_data[[1]])
+
+      if (nrow(subgct_z@mat) == 0) {
+        message(paste0("No rows found in ", the_file, ", skipping"))
+        return(NULL)
+      }
+      if (nrow(subgct_z@mat) > 499) {
+        message(paste("Too many rows in ", the_file, ", skipping"))
+        return(NULL)
+      }
+
+      # Generate heatmaps for this subset
+      param_grid %>%
+        purrr::pmap(~ generate_single_selection_heatmap(
+          subgct_z = subgct_z,
+          cut_by = ..1,
+          cluster_rows = ..2,
+          cluster_columns = ..3,
+          outdir = outdir,
+          outname = outname,
+          friendly_name = friendly_name,
+          file_name = file_name,
+          config = config
+        ))
+    })
+}
+
+
+generate_single_selection_heatmap <- function(subgct_z, cut_by, cluster_rows, cluster_columns, outdir, outname, file_name='', friendly_name = '', config) {
+
+  heatmap_tools <- get_tool_env("heatmap")
+
+  outf <- heatmap_tools$prepare_heatmap_output_path(outdir,
+    file.path("heatmap", "selection"),
+    outname,
+    prefix = file_name,
+    dimensions = list(nrow=nrow(subgct_z@mat), ncol(subgct_z@mat)),
+    cut_by = cut_by,
+    cluster_rows = cluster_rows,
+    cluster_columns = cluster_columns
+  )
+
+  if (file.exists(outf) && config$advanced$replace == FALSE) {
+    message(paste0(outf, " already exists, skipping"))
+    return(NULL)
+  }
+
+
+  # Generate heatmap code
+  ht_draw_code <- heatmap_tools$make_heatmap_fromgct(
+    gct = subgct_z,
+    column_title = file_name,
+    cut_by = cut_by,
+    cluster_rows = cluster_rows,
+    cluster_columns = cluster_columns,
+    meta_to_include = config$heatmap$legend_include %||% NULL,
+    meta_to_exclude = config$heatmap$legend_exclude %||% NULL,
+    show_row_names = TRUE,
+    include_row_annotations = TRUE,
+    # column_title = friendly_name,
+    optimal_size = TRUE
+
+  )
+  # Draw and save the heatmap
+  heatmap_tools$draw_and_save_heatmap(ht_draw_code, outf, subgct_z)
+}
+
 
 run <- function(
     data_dir = NULL,
@@ -66,15 +241,12 @@ run <- function(
   # suppressPackageStartupMessages(library(here))
   # print(paste0("here is defined as: ", here()))
 
-  source(file.path("setup_environment.R"))
-  source(file.path("lazyloader.R"))
   library(tictoc)
 
   tic("setting up env")
   setup_environment()  # populates the global environment with the tools and libraries
   toc(log=FALSE)
 
-  return()
 
   if (exists("get_tool_env", where = .GlobalEnv, mode = "function")){
     get_tool_env <- .GlobalEnv$get_tool_env
@@ -82,8 +254,8 @@ run <- function(
 
   io_tools <- get_tool_env("io")
   heatmap_tools <- get_tool_env("heatmap")
-  util_tools <- get_tool_env("utils")
-  limma_tools <- get_tool_env("limma")
+  # util_tools <- get_tool_env("utils")
+  # limma_tools <- get_tool_env("limma")
 
   OUTDIR <- output_dir %||% data_dir %||% "."
 
@@ -92,49 +264,13 @@ run <- function(
   config <- load_and_validate_config(config_file, data_dir)
   # config <- io_tools$load_config(config_file, root_dir = data_dir)
 
-
   print(config)
-
 
   gct_file <- config$gct_file %||% gct_file %||% NULL
   gct <- process_gct_file(gct_file, config)
   gct <- exclude_samples(gct, config$extra$sample_exclude)
   browser()
 
-  # gct_file <- file.path(data_dir %||% '.', config$gct_file) %||% gct_file %||% NULL
-
-  # if (!is.null(gct_file)) {
-  #   gct <- cmapR::parse_gctx(gct_file)
-  # } else {
-  #   cat("no data passed, exiting")
-  #   stop()
-  # }
-
-
-  # if (config$norm$normalize %||% FALSE){
-  #     print("normalizing..")
-  #     gct <- util_tools$normalize_gct(gct, log_transform=config$norm$log_transform %||% TRUE)
-  # }
-
-  # #if (config$$non_zeros %||% "median" == "median"){
-  # print("filtering...")
-  # gct <- util_tools$filter_nonzeros(gct,
-  #                                   config$filter$non_zeros %||% 1.0,
-  #                                   config$filter$nonzero_subgroup %||% NULL
-  #                                   )
-
-  # if (is.character(config$norm$batch) %||% FALSE){
-  #     gct <- util_tools$do_combat(gct, by=config$norm$batch)
-  # }
-
-
-  # if (!is.null(config$extra$sample_exclude)) {
-  #   to_exclude <- config$extra$sample_exclude %||% NULL
-  #   to_exclude <- intersect(rownames(gct@cdesc), to_exclude)
-  #   message(paste0("excluding ", length(to_exclude), " samples"))
-  #   to_keep <- setdiff(rownames(gct@cdesc), to_exclude)
-  #   gct <- gct %>% subset_gct(cid = to_keep)
-  # }
 
   suboutdir <- basename(gct_file) %>%
     basename() %>%
@@ -146,166 +282,29 @@ run <- function(
 
   FRIENDLY_NAME <- suboutdir %>% str_replace_all("_", " ") %>% str_replace(., "\\d+$", "") %>% str_wrap(width = 60)
 
-  gct_z <- util_tools$scale_gct(gct, group_by = config$heatmap$zscore_by)
+  util_tools <- get_tool_env("utils")
+  gct_z <- util_tools$scale_gct(gct, group_by = config$heatmap$zscore_by) %cached_by% rlang::hash(c(gct, config$heatmap$zscore_by))
+
 
 
   if (config$heatmap$selection$do %||% FALSE) {
+    # Heatmap generation for selections
+    generate_selection_heatmaps(gct_z, config, outdir=OUTDIR, friendly_name=FRIENDLY_NAME, outname=OUTNAME)
+  }
 
-    param_grid <- expand.grid(
-      cut_by = config$heatmap$selection$cut_by %||% config$heatmap$cut_by %||% NA,
-      cluster_rows = config$heatmap$selection$cluster_rows %||% config$heatmap$cluster_rows %||% TRUE,
-      cluster_columns = config$heatmap$selection$cluster_columns %||% config$heatmap$cluster_columns %||% c(TRUE, FALSE),
-      stringsAsFactors = FALSE
-    )
-
-
-    config$heatmap$selection$file_list %>%
-      purrr::list_flatten() %>%
-      imap(~{
-        the_file <- .x
-        file_name <- .y
-
-        file_data <- io_tools$read_genelist_file(the_file)
-
-        subgct_z <- gct_z %>% subset_gct(rid = file_data$id %||% file_data[[1]]) # fallback
-        print(paste('nrow submat gct: ', nrow(subgct_z@mat)))
-        if (nrow(subgct_z@mat) == 0) {
-          message(paste0("No rows found in ", the_file, ", skipping"))
-          return()
-        }
-        if (nrow(subgct_z@mat) > 499) {
-          message(paste("too many rows", the_file, ", skipping"))
-          print(length(file_data$id))
-          print(head(file_data))
-          print(file_data$id)
-          # browser()
-          return()
-        }
-        .nrow <- nrow(subgct_z@mat)
-        .ncol <- ncol(subgct_z@mat)
-        #
-        param_grid %>% purrr::pmap(~ {
-          cut_by_val <- ..1
-          cluster_rows_val <- ..2
-          cluster_columns_val <- ..3
-
-          .outdir <- file.path(OUTDIR, "heatmap", "selection", paste0("cut_", make.names(cut_by_val)))
-          if (!dir.exists(.outdir)) dir.create(.outdir, recursive = TRUE)
-          .outf <- file.path(
-            .outdir,
-            make.names(
-              paste0(
-                OUTNAME,
-                file_name,
-                "_",
-                .nrow, "x", .ncol,
-                "_cut_", cut_by_val,
-                "cr_", cluster_rows_val,
-                "cc_", cluster_columns_val,
-                ".pdf"
-              )
-            )
-          )
-
-          ht_draw_code <- heatmap_tools$make_heatmap_fromgct(
-            gct = subgct_z,
-            column_title = file_name,
-            cut_by = cut_by_val,
-            cluster_rows = cluster_rows_val,
-            cluster_columns = cluster_columns_val,
-            meta_to_include = config$heatmap$legend_include %||% NULL,
-            meta_to_exclude = config$heatmap$legend_exclude %||% NULL,
-            show_row_names = TRUE,
-            include_row_annotations = TRUE,
-            optimal_size = TRUE
-            )
-          height <- 6 + (.nrow * .20)
-          width <- 12 + (.ncol * .26)
-
-          message(paste0('drawing ', .outf))
-          if (file.exists(.outf) && config$advanced$replace == FALSE){
-            message(paste0(.outf, " already exists, skipping"))
-            return(NULL)
-          }
-          tryCatch(
-            {
-              cairo_pdf(.outf, width = width, height = height)
-              ht_draw_code()
-              dev.off()
-            },
-            error = function(e) print(e)
-          )
-        })
-      }) # end of imap of file list
-  } # end of if selection
-
-
-  param_grid <- expand.grid(
-    cut_by = unique(c(config$heatmap$cut_by, NA)),
-    cluster_columns = c(TRUE, FALSE),
-    stringsAsFactors = FALSE
-  )
 
   if (config$heatmap$do %||% FALSE) {
-    param_grid %>% purrr::pmap(~ {
-      .cut_by <- ..1
-      .cluster_columns <- ..2
-
-      # hack
-      # also not working
-      rids_to_keep <- gct_z@rdesc %>% distinct(fifteenmer, .keep_all = TRUE) %>% pull(id) #%>% sample(4002)
-      .gct_z <- gct_z %>% subset_gct(rid = rids_to_keep)
-
-      ht_draw_code <- .gct_z %>% heatmap_tools$make_heatmap_fromgct(
-        show_row_names = FALSE,
-        optimal_size = FALSE,
-        cut_by = .cut_by,
-        cluster_columns = .cluster_columns,
-        include_row_annotations = FALSE,
-        meta_to_include = config$heatmap$legend_include %||% NULL,
-        meta_to_exclude = config$heatmap$legend_exclude %||% NULL,
-        column_title=FRIENDLY_NAME
-      )
-
-      .nrow <- nrow(.gct_z@mat)
-      .ncol <- ncol(.gct_z@mat)
-      width <- 6.4 + (.ncol * .26)
-      height <- 7.8
-
-
-      .outdir <- file.path(OUTDIR, "heatmap")
-      if (!dir.exists(.outdir)) dir.create(.outdir, recursive = TRUE)
-      .outf <- file.path(
-        .outdir,
-        make.names(
-          paste0(
-            OUTNAME,
-            .nrow, "x", .ncol,
-            "_cut_", .cut_by,
-            "cc_", .cluster_columns,
-            ".pdf"
-          )
-        )
-      )
-
-      if (file.exists(.outf) && config$advanced$replace == FALSE) {
-        message(paste0(.outf, " already exists, skipping"))
-        return(NULL)
-      }
-      message(paste0("drawing ", .outf))
-      tryCatch(
-        {
-          cairo_pdf(.outf, width = width, height = height)
-          ht_draw_code()
-          dev.off()
-        },
-        error = function(e){ print("there was a problem drawing the heatmap"); print(e) }
-      )
-    }) # end of pmap of param grid
-  } # end of if heatmap
-
+    generate_global_heatmaps(
+      gct_z = gct_z,
+      config = config,
+      outdir = OUTDIR,
+      friendly_name = FRIENDLY_NAME,
+      outname = OUTNAME
+    )
+  }
 
   if (config$limma$do) {
+    limma_tools <- get_tool_env("limma")
     topTables <- limma_tools$run_limma(gct, config)
 
     topTables %>% purrr::imap(~ { # first loop save tables
