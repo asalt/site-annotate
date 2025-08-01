7a12e0cc9f2be04d40a468fef6873fb7689f714b
Alex Saltzman <saltzman@bcm.edu>
2025-08-01 11:20:57 -0500

    do not keep creating log handlers if they already exist

--- site_annotate/log.py
+++ site_annotate/log.py
@@ -1,29 +1,65 @@
+import os
 import logging
 
 
-def get_logger(name):
+# def get_logger(name):
 
-    # Create a logger object
+#     # Create a logger object
+#     logger = logging.getLogger(name)
+#     logger.setLevel(logging.INFO)  # Set the default logging level
+
+#     # Create handlers
+#     console_handler = logging.StreamHandler()
+#     file_handler = logging.FileHandler("app.log")
+
+#     # Set level for handlers
+#     console_handler.setLevel(logging.INFO)
+#     file_handler.setLevel(logging.INFO)
+
+#     # Create a logging format
+#     formatter = logging.Formatter(
+#         "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+#     )
+#     console_handler.setFormatter(formatter)
+#     file_handler.setFormatter(formatter)
+
+#     # Add handlers to the logger
+#     logger.addHandler(console_handler)
+#     # logger.addHandler(file_handler)
+
+#     return logger
+
+
+def get_logger(name: str, log_file: str = "app.log") -> logging.Logger:
     logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)  # Set the default logging level
 
-    # Create handlers
+    if logger.hasHandlers():
+        return logger  # Prevent duplicate handlers if already configured
+
+    # logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)
+
+    # Console handler
     console_handler = logging.StreamHandler()
-    file_handler = logging.FileHandler("app.log")
+    # console_handler.setLevel(logging.INFO)
+    console_handler.setLevel(logging.DEBUG)
 
-    # Set level for handlers
-    console_handler.setLevel(logging.INFO)
+    # File handler (create log directory if needed)
+    log_dir = os.path.dirname(log_file)
+    if log_dir and not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+    file_handler = logging.FileHandler(log_file, mode="a")
     file_handler.setLevel(logging.INFO)
 
-    # Create a logging format
+    # Formatter
     formatter = logging.Formatter(
         "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     )
     console_handler.setFormatter(formatter)
     file_handler.setFormatter(formatter)
 
-    # Add handlers to the logger
+    # Add handlers
     logger.addHandler(console_handler)
-    # logger.addHandler(file_handler)
+    logger.addHandler(file_handler)
 
     return logger
